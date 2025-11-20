2fca3bedb1b1389dd93f86f1a3fc737a793a804f
higgsboson909 <higgsbosson906@gmail.com>
2025-11-21 00:48:35 +0500

    App file

---
+++ app.py
@@ -0,0 +1,49 @@
+import os
+import json
+from request import download_json_file
+from semester_table import semester_table
+
+JSON_URL = "https://griffin-k.github.io/Time-Table-Actions/read-data.json" 
+
+def data(): 
+    file_path = "./data.json"
+
+    if os.path.isfile(file_path):
+        print(f"The file '{file_path}' exists and is a regular file.")
+        return True
+    else:
+        print(f"The file '{file_path}' does not exist or is not a regular file.")
+        return False 
+
+
+def semester_data():
+    file_path = "./semester_table_data.json"
+
+    if os.path.isfile(file_path):
+        print(f"The file '{file_path}' exists and is a regular file.")
+        return True
+    else:
+        print(f"The file '{file_path}' does not exist or its not a regualr file.")
+        return False
+
+
+if (not data()):
+    download_json_file(JSON_URL, "data.json")
+
+
+if (not semester_data()):
+    # Define the file name
+    filename = 'data.json'
+
+    # Open the file and load the JSON data
+    try:
+        with open(filename, 'r') as file:
+            data = json.load(file)
+        
+        semester_table_data = semester_table(data)
+
+    except FileNotFoundError:
+        print(f"Error: The file '{filename}' was not found.")
+    except json.JSONDecodeError as e:
+        print(f"Error: Failed to decode JSON from the file. Details: {e}")
+
